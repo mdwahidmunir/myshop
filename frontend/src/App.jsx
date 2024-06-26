674f85e7d62469bd27cb1b65526db930739db3c6
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-06-26 15:09:13 +0530

    Added Home Screen and Product lists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
 import Header from "./components/Navbar";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
-        <h2>Hello World</h2>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
