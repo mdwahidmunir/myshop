ed0a37d99d80618bdb4c452094950786bc929402
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-07-15 21:51:43 +0530

    Added auth controller and middlewares

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,20 +2,20 @@ const nodeMailer = require('nodemailer')
 const path = require('path')
 const dotenv = require('dotenv')
 const fs = require('fs')
-const generateOTP = require('./otpGenerator')
-
 const { WELCOME_TEMPLATE, OTP_TEMPLATE } = require('./templates')
 
-dotenv.config({ path: path.join('..', '.env') })
+// dotenv.config({ path: path.join('..', '.env') }) // Use it only when you want to run this module separately
+dotenv.config()
 
-const { EMAIL, APP_PASS } = process.env
+
+const { SENDER_EMAIL, APP_PASS } = process.env
 
 const transporter = nodeMailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-        user: EMAIL,
+        user: SENDER_EMAIL,
         pass: APP_PASS
     }
 })
@@ -28,23 +28,26 @@ const replaceContent = (content, kwargs) => {
     return content
 }
 
-const emailHelper = async (transporter, template, kwargs) => {
+const emailHelper = async (template, mailRecepient, kwargs) => {
     try {
         const template_path = path.join(__dirname, 'email_templates', template)
         const content = await fs.promises.readFile(template_path, 'utf-8')
 
         const mailOptions = {
-            to: EMAIL,
-            from: EMAIL,
+            to: mailRecepient,
+            from: SENDER_EMAIL,
             subject: 'Welcome to MyShop',
             html: replaceContent(content, kwargs)
         }
 
         await transporter.sendMail(mailOptions)
+        console.log("Mail Succesfully Sent to recepient/s :: ", mailRecepient)
     }
     catch (err) {
         console.log(err)
     }
 }
 
-emailHelper(transporter, OTP_TEMPLATE, { username: 'Wahid', otp: generateOTP() })
+module.exports = emailHelper
+
+// emailHelper(WELCOME_TEMPLATE, SENDER_EMAIL, { username: 'Wahid' })
