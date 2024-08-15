e4fff205ea070bbdcf46ba594ec225c13e438bdd
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-08-15 18:59:07 +0530

    Readding the port in cors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ mongoose.connect(dbURL).then((conn) => {
 const app = express();
 
 const corsOptions = {
-    origin: `${FRONTEND_URL}`,
+    origin: `${FRONTEND_URL}:${FRONTEND_PORT}`,
     credentials: true,
 }
 
