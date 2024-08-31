6eaeb22ca52afe570a9c8a2c5a7bc43443c332ad
mdwahidmunir <mdwahid.munir1@gmail.com>
2024-09-01 00:29:06 +0530

    Added back button

--- frontend/src/screens/OrderDetailsScreen.jsx
+++ frontend/src/screens/OrderDetailsScreen.jsx
@@ -1,14 +1,22 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import OrderDetails from "../components/OrderDetails";
+import { useNavigate } from "react-router-dom";
 
 function OrderDetailsScreen() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(-1);
+  };
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
+          <Button onClick={handleClick} className="btn btn-light my-3">
+            Go Back
+          </Button>
           <OrderDetails />
         </Container>
       </main>
